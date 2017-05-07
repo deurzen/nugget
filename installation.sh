042d8994eba3065dfc6df0b71fa7024056ff7e81
Max <m.vandeurzen@student.ru.nl>
2017-05-07 20:21:22 +0200

    Adds installation script

--- installation.sh
+++ installation.sh
@@ -3,7 +3,7 @@ $DIR="/var/lib/vimsg/"
 $NUGGET_DIR="${DIR}nugget"
 sudo mkdir -p $NUGGET_DIR 2>/dev/null
 sudo chown -R $USER $DIR
-chmod +x ./nugget.pl
-sudo mv ./nugget.pl /usr/bin/nugget
+chmod u+x ./nugget.pl
+sudo mv ./nugget.pl /usr/local/bin/nugget
 echo "nugget successfully installed"
 nugget
