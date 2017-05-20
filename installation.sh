7e9cb939b9aac78983befc68962e19adb42855f4
Max <m.vandeurzen@student.ru.nl>
2017-05-21 01:37:03 +0200

    Updates installation method

--- installation.sh
+++ installation.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 $DIR="/var/lib/vimsg/"
 $NUGGET_DIR="${DIR}nugget"
+$DELETED_DIR="${DIR}deleted"
 sudo mkdir -p $NUGGET_DIR 2>/dev/null
+sudo mkdir -p $DELETED_DIR 2>/dev/null
 sudo chown -R $USER $DIR
-chmod u+x ./nugget.pl
-sudo install ./nugget.pl /usr/local/bin/nugget
+pp -o nugget nugget.pl
+sudo install nugget /usr/local/bin/
 echo "application successfully installed"
 nugget
